a863da4ab9ca769689a38c496b707ad35cc126d1
Roshan-Pagar <roshanpagar10@gmail.com>
2024-11-05 23:11:27 +0530

    Ai_interview

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import img1 from '../assets/img1.jpg'
-import img2 from '../assets/img2.jpg'
-import img3 from '../assets/img3.jpg'
+import img1 from '../assets/Resume2.jpg'
+import img2 from '../assets/Tech1.jpg'
+import img3 from '../assets/Interview3.jpg'
 const Cards = () => {
   return (
     <div className="flex justify-center flex-wrap gap-6 mt-24">
@@ -14,7 +14,7 @@ const Cards = () => {
           className="w-full h-auto object-cover"
         />
         <div className="p-5">
-          <h1 className="text-xl mb-2 text-center">Card Title 1</h1>
+          <h1 className="text-xl mb-2 text-center">Resume Optimization</h1>
           <p className="text-sm text-gray-600 mb-5">Description for the first card goes here.</p>
           <a href="#" className="inline-block bg-blue-600 text-white rounded px-4 py-2">
             Learn More
@@ -30,7 +30,7 @@ const Cards = () => {
           className="w-full h-auto object-cover"
         />
         <div className="p-5">
-          <h1 className="text-xl mb-2 text-center">Card Title 2</h1>
+          <h1 className="text-xl mb-2 text-center">Technical Interview</h1>
           <p className="text-sm text-gray-600 mb-5">Description for the second card goes here.</p>
           <a href="#" className="inline-block bg-blue-600 text-white rounded px-4 py-2">
             Learn More
@@ -46,7 +46,7 @@ const Cards = () => {
           className="w-full h-auto object-cover"
         />
         <div className="p-5">
-          <h1 className="text-xl mb-2 text-center">Card Title 3</h1>
+          <h1 className="text-xl mb-2 text-center">Hr Interview</h1>
           <p className="text-sm text-gray-600 mb-5">Description for the third card goes here.</p>
           <a href="#" className="inline-block bg-blue-600 text-white rounded px-4 py-2">
             Learn More
